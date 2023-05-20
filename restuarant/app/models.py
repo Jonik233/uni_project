584c1f75fa704839de7a2d6643bf13b90e73297b
Vlad Mashtalier <vmashtaler5@gmail.com>
2023-05-20 14:39:02 +0300

    V4

--- restuarant/app/models.py
+++ restuarant/app/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+
+class ReservationProfile(models.Model):
+    visitor_name = models.CharField(max_length=200)
+    visitor_phone_number = models.IntegerField()
+    date = models.DateField(auto_now_add=False)
+    time = models.TimeField(auto_now_add=False)
+    comments = models.TextField()
+    
+class Dish(models.Model):
+    title = models.CharField(max_length=200)  
+    description = models.TextField(max_length=500)
+    weight = models.IntegerField()
+    price = models.IntegerField()
